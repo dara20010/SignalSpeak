f788ae448d629bbde5e8ab20cda8d53af5260206
Dara <dara20010@hotmail.com>
2023-12-14 10:19:26 -0500

    Modificacion

--- src/app/features/tools/components/cam-detector/cam-detector.component.ts
+++ src/app/features/tools/components/cam-detector/cam-detector.component.ts
@@ -23,7 +23,7 @@ export class CamDetectorComponent implements OnInit, OnDestroy {
   private intervalId?: number;
 
 
-  mockedWord: string = 'ROSA';
+  mockedWord: string = 'HOLA';
   mockedWordIndex: number = 0;
   private intervalTime: number = 2000;
 
